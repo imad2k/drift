9fc71535363c30754cf99e0d7ffcb2dbe1e80fdd
Imad <iabusam@gmail.com>
2025-01-18 09:50:19 -0600

    finished the ger_fundamentals function

--- backend/services/data_ingestion.py
+++ backend/services/data_ingestion.py
@@ -7,17 +7,33 @@ from pprint import pprint
 # Load the API key from the .env file
 load_dotenv()
 
+# Get the API key from the environment variables
 EOD_API_KEY = os.getenv('EOD_API_KEY')
 if EOD_API_KEY is None:
     raise ValueError("EOD_API_KEY is not set in the environment variables")
 
+# Create an instance of the APIClient
 eod_api = APIClient(api_key=EOD_API_KEY)
 
 ticker = input("Enter the ticker: ")
 
 def get_fundamentals_data(ticker):
     data = eod_api.get_fundamentals_data(ticker)
-    pprint(data)
+    
+    #This is the general data
+    company_code = data['General']['Code']
+    company_name = data['General']['Name']
+    company_sector = data['General']['Sector']
+    
+    highlights = data["Highlights"]
+    valuation = data["Valuation"]
+    technicals = data["Technicals"]
+    earnings = data["Earnings"]
+    financials = data["Financials"]
+    
+    pprint({'company highlights: ':highlights, 
+            })
+    
 
 get_fundamentals_data(ticker)
 
